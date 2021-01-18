f0b367b35359b49b6594fdd21d304d47f3cce616
Julian <jchumtl@gmail.com>
2021-01-17 19:23:58 -0500

    Removing Spotify authentitation code

--- src/SpotifyAuth.js
+++ src/SpotifyAuth.js
@@ -1,10 +1,10 @@
 // Required params for Spotify implicit grant flow:
 // Don't need scopes due to searching Spotify information, not user information
 let authEndpoint = "https://accounts.spotify.com/authorize";
-let clientId = "3f61b0a7aa7940b4ac9abbb26481030f";
+let clientId = "";
 let redirectUri = "http://localhost:8888";
 let responseType = "token";
-// let state = "123";
+// let state = "123"; // Optional parameter
 
 let spotifyURL = authEndpoint + "?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&response_type=" + responseType + "&show_dialog=true";
 
