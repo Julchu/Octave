8ead2dd96e86a5ad439b467397f62dfe2b0fd534
Julian <jchumtl@gmail.com>
2021-01-19 22:39:53 -0500

    Removing Heroku integration

--- src/SpotifyAuth.js
+++ src/SpotifyAuth.js
@@ -1,16 +1,7 @@
-// Getting Heroku URI
-
-// let uri = "";
-// if (typeof process.env.clientId == 'undefined') {
-// 	uri = "3f61b0a7aa7940b4ac9abbb26481030f";
-// } else {
-// 	uri = process.env.clientId;
-// }
-
 // Required params for Spotify implicit grant flow:
 // Don't need scopes due to searching Spotify information, not user information
 let authEndpoint = "https://accounts.spotify.com/authorize";
-let clientId = process.env.clientId;
+let clientId = "";
 let redirectUri = "http://localhost:8888";
 let responseType = "token";
 // let state = "123"; // Optional parameter
